27ee4e7aa954cfa5f79bff89a9ce3fb8d11a2c9a
Lucas Block <lucas.block@mulesoft.com>
2018-03-15 12:16:06 -0300

    build plugin info

--- src/main/scala/org/mule/util/DependenciesUtils.scala
+++ src/main/scala/org/mule/util/DependenciesUtils.scala
@@ -1,12 +1,12 @@
 package org.mule.util
 
-//import AMFMeter.BuildInfo
+import AMFMeter.BuildInfo
 
 object DependenciesUtils {
 
-//  def getDependencyVersion(dependencyName: String): String = {
-//    val dependencies = BuildInfo.allDependencies.map(dl => dl.split(':')).map(d => d(1) -> d(2)).toMap
-//    dependencies(dependencyName)
-//  }
+  def getDependencyVersion(dependencyName: String): String = {
+    val dependencies = BuildInfo.allDependencies.map(dl => dl.split(':')).map(d => d(1) -> d(2)).toMap
+    dependencies(dependencyName)
+  }
 
 }
